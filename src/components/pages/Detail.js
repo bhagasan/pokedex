7cc2c91f262ce8b9ac7ebc38c9b0062d9dd11318
bhagasan <bhagasan@nodeflux.io>
2020-04-09 16:03:25 +0700

    get list pokemons

---
+++ src/components/pages/Detail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styled from "styled-components";
+
+export default function Detail(props) {
+  return (
+    <Wrapper>
+      <h1>Detail</h1>
+    </Wrapper>
+  );
+}
+
+const Wrapper = Styled.div``;
---
+++ src/components/pages/SplashScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styled from "styled-components";
+
+export default function SplashScreen(props) {
+  return (
+    <Wrapper>
+      <h1>Splashscreen</h1>
+    </Wrapper>
+  );
+}
+
+const Wrapper = Styled.div``;
